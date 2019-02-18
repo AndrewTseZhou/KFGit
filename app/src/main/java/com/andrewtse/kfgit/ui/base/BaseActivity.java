21e30fe28d91c38252fa582730d4c3946284c882
谢康 <xk@meitu.com>
2019-02-18 20:44:11 +0800

    动态权限申请

--- app/src/main/java/com/andrewtse/kfgit/ui/base/BaseActivity.java
+++ app/src/main/java/com/andrewtse/kfgit/ui/base/BaseActivity.java
@@ -1,9 +1,15 @@
 package com.andrewtse.kfgit.ui.base;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 /**
  * @author xk
@@ -11,8 +17,40 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class BaseActivity extends AppCompatActivity {
 
+    public static final int REQUEST_STORAGE_PERMISSION = 100;
+    public static final int REQUEST_PHONE_PERMISSION = 101;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE_PERMISSION);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_PHONE_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            switch (requestCode) {
+                case REQUEST_PHONE_PERMISSION:
+                    Toast.makeText(this, "申请电话权限成功", Toast.LENGTH_SHORT).show();
+                    break;
+                case REQUEST_STORAGE_PERMISSION:
+                    Toast.makeText(this, "申请外部存储权限成功", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
