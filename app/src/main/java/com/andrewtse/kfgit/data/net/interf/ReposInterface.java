f5f705a4ec1e913724015bba7070204f1393f894
AndrewTse <xkyx007@163.com>
2019-03-24 17:45:26 +0800

    增加其他功能的接口

--- app/src/main/java/com/andrewtse/kfgit/data/net/interf/ReposInterface.java
+++ app/src/main/java/com/andrewtse/kfgit/data/net/interf/ReposInterface.java
@@ -1,14 +1,20 @@
 package com.andrewtse.kfgit.data.net.interf;
 
 import com.andrewtse.kfgit.data.net.response.SearchResp;
+import com.andrewtse.kfgit.model.BaseRepoModel;
 import com.andrewtse.kfgit.model.RepoDetailInfoModel;
 import com.andrewtse.kfgit.model.StarredModel;
+import com.andrewtse.kfgit.model.User;
 
 import java.util.List;
 
 import io.reactivex.Observable;
+import okhttp3.ResponseBody;
+import retrofit2.Response;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -41,4 +47,36 @@ public interface ReposInterface {
     @Headers("Cache-Control: public, max-age=3600")
     @GET("repos/{owner}/{repo}")
     Observable<RepoDetailInfoModel> getRepoDetailInfo(@Path("owner") String owner, @Path("repo") String repo);
+
+    @Headers("Cache-Control: public, max-age=600")
+    @GET("users/{name}/starred")
+    Observable<List<StarredModel>> getUserStarredRepos(@Path("name") String user);
+
+    //获取一个仓库的所有贡献者
+    @Headers("Cache-Control: public, max-age=3600")
+    @GET("repos/{owner}/{name}/contributors")
+    Observable<List<User>> getContributors(@Path("owner") String owner, @Path("name") String repo);
+
+    //获取我的所有仓库
+    @Headers("Cache-Control: public, max-age=600")
+    @GET("user/repos")
+    Observable<List<BaseRepoModel>> getMyRepos(@Query("type") String type);
+
+    //获取某个用户所有的仓库
+    @Headers("Cache-Control: public, max-age=600")
+    @GET("users/{name}/repos")
+    Observable<List<BaseRepoModel>> getUserRepos(@Path("name") String user);
+
+    //Star一个仓库
+    @Headers("Content-Length: 0")
+    @PUT("/user/starred/{owner}/{repo}")
+    Observable<Response<ResponseBody>> starRepo(@Path("owner") String owner, @Path("repo") String repo);
+
+    //UnStar一个仓库
+    @DELETE("/user/starred/{owner}/{repo}")
+    Observable<Response<ResponseBody>> unstarRepo(@Path("owner") String owner, @Path("repo") String repo);
+
+    //检查一个仓库是否被star
+    @GET("/user/starred/{owner}/{repo}")
+    Observable<Response<ResponseBody>> checkIfRepoIsStarred(@Path("owner") String owner, @Path("repo") String repo);
 }
--- app/src/main/java/com/andrewtse/kfgit/data/net/interf/UserInterface.java
+++ app/src/main/java/com/andrewtse/kfgit/data/net/interf/UserInterface.java
@@ -4,10 +4,14 @@ import com.andrewtse.kfgit.data.net.request.CreateAuthorization;
 import com.andrewtse.kfgit.data.net.response.AuthorizationResp;
 import com.andrewtse.kfgit.model.User;
 
+import java.util.List;
+
 import io.reactivex.Observable;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -21,4 +25,20 @@ public interface UserInterface {
 
     @GET("/user")
     Observable<User> getUserInfo(@Query("access_token") String accessToken);
+
+    @Headers("Cache-Control: public, max-age=3600")
+    @GET("/user/following")
+    Observable<List<User>> getMyFollowing();
+
+    @Headers("Cache-Control: public, max-age=3600")
+    @GET("/users/{user}/following")
+    Observable<List<User>> getUserFollowing(@Path("user") String user);
+
+    @Headers("Cache-Control: public, max-age=3600")
+    @GET("/user/followers")
+    Observable<List<User>> getMyFollowers();
+
+    @Headers("Cache-Control: public, max-age=3600")
+    @GET("/users/{user}/followers")
+    Observable<List<User>> getUserFollowers(@Path("user") String user);
 }
